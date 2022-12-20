16425706ea1fa37bd2a1ffe55286598d4ad6e2dc
Smith <111470120+Smith2067@users.noreply.github.com>
2022-12-20 13:40:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function home(){
-    window.location("https://www.google.com");
-    window.location.href = "https://www.google.com";
+    window.location("https://smith2067.github.io/Jquery-home/");
+    window.location.href = "https://smith2067.github.io/Jquery-home/";
   }
   function vol(){
-    window.location("https://www.google.com");
-    window.location.href = "https://www.google.com";
+    window.location("https://smith2067.github.io/Jquery-volunteer/");
+    window.location.href = "https://smith2067.github.io/Jquery-volunteer/";
   }
   function donate(){
-    window.location("https://www.google.com");
-    window.location.href = "https://www.google.com";
+    window.location("https://smith2067.github.io/Jquery-donate/");
+    window.location.href = "https://smith2067.github.io/Jquery-donate/";
   }
   function faq(){
-    window.location("https://www.google.com");
-    window.location.href = "https://www.google.com";
+    window.location("https://smith2067.github.io/Jquery-faq/");
+    window.location.href = "https://smith2067.github.io/Jquery-faq/";
   }
